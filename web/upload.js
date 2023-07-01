5c600ceafa2b714301eefa122215303a86af99ad
Sunny2567 <133204687+Sunny2567@users.noreply.github.com>
2023-07-01 17:02:24 +0800

    修正项目结构

---
+++ web/upload.js
@@ -0,0 +1,16 @@
+function uploadFile() {
+    var fileInput = document.getElementById('file');
+    var file = fileInput.files[0];
+
+    var formData = new FormData();
+    formData.append('file', file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/Upload', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log('File uploaded successfully.');
+        }
+    };
+    xhr.send(formData);
+}
