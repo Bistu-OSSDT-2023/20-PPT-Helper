152fbc0f8f6f18590ffa3815e01da9428217cb46
Sunny2567 <133204687+Sunny2567@users.noreply.github.com>
2023-07-01 16:13:42 +0800

    更新UploadServlet，改用apache.commons.fileupload

--- src/main/java/org/example/FileUploadServlet.java
+++ src/main/java/org/example/FileUploadServlet.java
@@ -1,52 +1,50 @@
-package org.example; /**
- * Created by sunday on 2023/7/1 3:45 PM.
- * Author: sunday
- * Description:
- */
+package org.example;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.disk.*;
+import org.apache.commons.fileupload.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.*;
+import javax.servlet.http.*;
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @WebServlet(name = "FileUploadServlet", value = "/Upload")
+@MultipartConfig
 public class FileUploadServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Part filePart = request.getPart("file");
-        String fileName = getFileName(filePart);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
 
-        // 指定本地保存文件的路径
-        String uploadDir = "./files/";
-        String filePath = uploadDir + fileName;
+        try {
+            List<FileItem> items = upload.parseRequest(request);
+            for (FileItem item : items) {
+                if (!item.isFormField() && item.getFieldName().equals("file")) {
+                    String fileName = item.getName();
 
-        // 创建本地文件
-        File dest = new File(filePath);
-        try (InputStream fileContent = filePart.getInputStream();
-             FileOutputStream fos = new FileOutputStream(dest)) {
-            int read;
-            byte[] buffer = new byte[1024];
-            while ((read = fileContent.read(buffer)) != -1) {
-                fos.write(buffer, 0, read);
+                    // 在文件名后添加时间戳
+                    String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+                    String updatedFileName = fileName + "_" + timeStamp;
+
+                    // 指定本地保存文件的路径
+                    String uploadDir = "./files/";
+                    String filePath = uploadDir + updatedFileName;
+
+                    // 创建本地文件
+                    File dest = new File(filePath);
+                    item.write(dest);
+                }
             }
-        }
 
-        response.setStatus(HttpServletResponse.SC_OK);
-    }
-}
-    private String getFileName(Part part) {
-        String contentDisposition = part.getHeader("content-disposition");
-        String[] elements = contentDisposition.split(";");
-        for (String element : elements) {
-            if (element.trim().startsWith("filename")) {
-                return element.substring(element.indexOf('=') + 1).trim().replace("\"", "");
-            }
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        return null;
     }
 }
