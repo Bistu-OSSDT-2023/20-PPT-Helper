20235b745b647f2b274ae6fb3351e0ef783aad00
Sunny2567 <133204687+Sunny2567@users.noreply.github.com>
2023-07-03 13:50:19 +0800

    优化代码结构方便复用，删除冗余

--- src/main/java/GLM.java
+++ src/main/java/GLM.java
@@ -10,9 +10,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GLM {
-    public static void main(String[] args) {
-        // 创建一个HttpClient对象
-        HttpClient client = HttpClient.newHttpClient();
+    public static HttpRequest get_Request(String mode){
+        // 创建一个JSON字符串，作为请求体
+        String jsonRequestBody=get_jsonRequestBody();
+        // 创建一个HttpRequest对象
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_lite/"+mode)) // 将这里替换为你的API URL
+                .header("Authorization", "eyJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04iLCJ0eXAiOiJKV1QifQ.eyJhcGlfa2V5IjoiZTE5MTY4ZTczYWMzYTQ5OWQyYjRiYWNlMzA4YTcxNDciLCJleHAiOjE2ODgyOTU1NTk2NzMsInRpbWVzdGFtcCI6MTY4ODI5MTk1OTY3M30.A4gDe0n-FQNpSiP9f98akstLAFC7W-NbaLdifdp3sfQ") // 替换为你的Authorization String
+                .header("Content-Type", "application/json")
+                .POST(BodyPublishers.ofString(jsonRequestBody)) // 使用JSON字符串创建请求体
+                .build();
+        return request;
+    }
+    public static String get_jsonRequestBody(){
+
         // 创建一个ObjectMapper对象，用于处理JSON
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -30,17 +41,19 @@ public class GLM {
         try {
             // 使用ObjectMapper将Map转换成JSON字符串
             jsonRequestBody = objectMapper.writeValueAsString(requestBody);
+            return jsonRequestBody;
         } catch (Exception e) {
             throw new RuntimeException("Failed to convert map to JSON", e);
         }
+    }
+    public static void main(String[] args) {
 
+        // 创建一个HttpClient对象
+        HttpClient client = HttpClient.newHttpClient();
+        // 创建一个JSON字符串，作为请求体
+        String jsonRequestBody=get_jsonRequestBody();
         // 创建一个HttpRequest对象
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_lite/invoke")) // 将这里替换为你的API URL
-                .header("Authorization", "eyJhbGciOi6IlNJR04iLCJ0eXAiOiJKV1QifQ.eyJhcGlfa2V5TczYWMzYTQ5OWQyYjRiYWNlMzA4YTcxNDciLCJleHAiOjE2ODgyOTU1NTk2NzMsInRpbWVzdGFtcCI6MTY4ODI5MTk1OTY3M30.A4gDe0n-FQNpSiP9f98akstLAFC7W-NbaLdifdp3sfQ") // 替换为你的Authorization String
-                .header("Content-Type", "application/json")
-                .POST(BodyPublishers.ofString(jsonRequestBody)) // 使用JSON字符串创建请求体
-                .build();
+        HttpRequest request = get_Request("invoke");
 
         try {
             // 使用HttpClient发送请求，并获取HttpResponse对象
